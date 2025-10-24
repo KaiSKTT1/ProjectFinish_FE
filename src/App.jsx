4a15dfe4dcb820a3ace981cd7508d2aefa508b39
kaiST <phucnguyen17072005@gmail.com>
2025-10-24 18:13:11 +0700

    create reactjs

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.scss'
+
+
+
+
+class App extends React.Component {
+
+
+  render() {
+    return (
+      <div className='app-container'>
+        <h1> Vite + React</h1>
+      </div >
+
+    )
+  }
+}
+
+
+export default App
